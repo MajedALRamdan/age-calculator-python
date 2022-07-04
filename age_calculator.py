839a8b937a4e373e81dc9253be0f8cd2bd6522ce
MajedALRamdan <majooddark237514@gmail.com>
2022-07-04 20:29:58 +0300

    age calculator

--- age_calculator.py
+++ age_calculator.py
@@ -1,14 +1,26 @@
 from datetime import date
 
 
-def get_dob():
+def get_dob(year, month, day):
     # write code here
 	...
+year=int(input("Enter year of birth: "))
+month=int(input("Enter month of birth: "))
+day=int(input("Enter day of birth:"))
+dob=date(year,month,day)
 
 
 def get_age(dob):
     # write code here
 	...
+today=date.today()
+age=today-dob
+aiy=str(age.days//365)
+if age.days//365<0:
+	print("Invalid date of birth")
+else:
+	print(f"You are {aiy} +years old")
+ 
 
 
 def main():
